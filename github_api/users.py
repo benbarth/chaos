d82057671f8ff141aaa98cad628d1e82f4b7c18f
Ben Barth <benbarth@users.noreply.github.com>
2017-05-25 21:03:16 -0400

    Add unfollow api

--- github_api/users.py
+++ github_api/users.py
@@ -4,7 +4,10 @@ def get_user(api, user):
     data = api("get", path)
     return data
 
-
 def follow_user(api, user):
     follow_path = "/user/following/{user}".format(user=user)
     follow_resp = api("PUT", follow_path)
+
+def unfollow_user(api, user):
+    unfollow_path = "/user/following/{user}".format(user=user)
+    unfollow_resp = api("DELETE", unfollow_path)
